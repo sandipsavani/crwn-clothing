09bc7a75b14ea09670b04964f14d181aa60cc7b8
Sandip Savani <savani045@gmail.com>
2022-02-01 19:00:44 +0530

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from "react";
-
-<<<<<<< HEAD
 import HomePage from "./pages/homepage/homepage.component";
-=======
-import HomePage from "./homepage.component";
->>>>>>> b7f0e1e795dc5723d0b431b4008a2ee63faeaad7
-
 import "./App.css";
 
 class App extends React.Component {
