37bd563762a9f264d2c4293b9b3283ffb564aea4
Sandip Savani <savani045@gmail.com>
2022-02-11 18:36:20 +0530

    Updateing Fav icon

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 import reportWebVitals from "./reportWebVitals";
 
@@ -14,7 +15,9 @@ ReactDOM.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   // </React.StrictMode>,
