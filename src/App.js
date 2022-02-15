b515a0e5539e172c152e109cdd9bbeb87da44dd7
Sandip Savani <savani045@gmail.com>
2022-02-15 18:41:59 +0530

    Implement a spinner on shop page usign HOC

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import "./App.css";
+
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -10,10 +12,11 @@ import CheckoutPage from "./pages/checkout/checkout.component";
 
 import Header from "./components/header/header.component";
 
-import { setCurrentUser } from "./redux/user/user.actions";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
+import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
-import "./App.css";
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -48,7 +51,6 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route exact path="/checkout" component={CheckoutPage} />
-
           <Route
             exact
             path="/signin"
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,17 @@
+import ShopActionTypes from "./shop.types";
 import SHOP_DATA from "./shop.data";
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA,
+  collections: null || SHOP_DATA,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionTypes.UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload,
+      };
     default:
       return state;
   }
